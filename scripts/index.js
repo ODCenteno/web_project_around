493d48ac16efc57d86e3127a8b4c3f4553a4d259
= <pixie_cemodan@hotmail.com>
2025-03-25 10:32:21 -0700

    feature: buttons Edit profile and close popup I/O bahavior

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,13 @@
+const editButton = document.querySelector("#button-edit");
 const popup = document.querySelector(".popup");
 const formDetailsResetBtn = document.querySelector("#popup-button-close");
 const formInputName = document.querySelector("#form-name");
 const formInputDescription = document.querySelector("#form-description");
 const formDetailsSubmitBtn = document.querySelector("#popup-button-close");
+
+editButton.addEventListener("click", (e) => {
+  popup.classList.remove("popup_hidden");
+});
 
 formDetailsResetBtn.addEventListener("click", (e) => {
   popup.classList.add("popup_hidden");
