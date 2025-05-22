78217eb3ccf8aaf8832a7f031f8614837671bf96
= <pixie_cemodan@hotmail.com>
2025-05-21 18:40:17 -0700

    refactor: baseArticleHTML function using DOM nodes

--- scripts/articles.js
+++ scripts/articles.js
@@ -50,25 +50,47 @@ let articlesContent = [
 ];
 
 const baseArticleHTML = (article) => {
-  return `
-  <article class="card articles__card">
-    <picture class="card__picture">
-      <img src="${article.imageUrl}" alt="${article.imageAlt}" class="card__image">
-    </picture>
-    <div class="card__place-info">
-      <h3 class="card__place-title">${article.articleTitle}</h3>
-      <div class="card__icon-container">
-        <img src="${article.iconUrl}" alt="like icon" data-isLiked=${article.isLiked} class="card__like-icon">
-      </div>
-    </div>
-  </article>
-`;
+  const articleNode = document.createElement("article");
+  articleNode.classList.add("card", "articles__card");
+  const pictureNode = document.createElement("picture");
+  pictureNode.classList.add("card__picture");
+  const imgNode = document.createElement("img");
+  imgNode.classList.add("card__image");
+
+  imgNode.setAttribute("src", `${article.imageUrl}`);
+  imgNode.setAttribute("alt", `${article.imageAlt}`);
+
+  pictureNode.append(imgNode);
+
+  const divNode = document.createElement("div");
+  divNode.classList.add("card__place-info");
+  const h3Node = document.createElement("h3");
+  h3Node.classList.add("card__place-title");
+  h3Node.textContent = article.articleTitle;
+  const iconContainerNode = document.createElement("div");
+  iconContainerNode.classList.add("card__icon-container");
+
+  const iconNode = document.createElement("img");
+  iconNode.classList.add("card__like-icon");
+  iconNode.setAttribute("src", article.iconUrl);
+  iconNode.setAttribute("alt", "like icon");
+  iconNode.setAttribute("data-isLiked", article.isLiked);
+  iconContainerNode.append(iconNode);
+
+  divNode.append(h3Node);
+  divNode.append(iconContainerNode);
+
+  articleNode.append(pictureNode);
+  articleNode.append(divNode);
+
+  return articleNode;
 };
 
 const articles = document.querySelector("#articles");
 
+// Actualizar los métodos para agregar una nueva tarjeta y quirar insertAdjacentHTML
 articlesContent.forEach((article) => {
-  articles.insertAdjacentHTML("beforeend", baseArticleHTML(article));
+  articles.prepend(baseArticleHTML(article));
 });
 
 articles.addEventListener("click", (event) => {
@@ -86,3 +108,11 @@ articles.addEventListener("click", (event) => {
     }
   }
 });
+
+// Agregar nuevo controlador para el botón de abrir modal Add New Place
+
+// Lógica para el manejo de formulario Add New place y agregar nueva tarjeta
+
+// Agregar controlador para eliminar una tarjeta
+
+// Controlador para abrir una ventana emergente con la imagen ampliada y su nombre como caption
