b1a7dd0b66471b286a837132677fc7acc265b6f7
ODCenteno <ODCenteno@users.noreply.github.com>
2025-08-08 20:28:01 +0000

    deploy: 658cf2a9c894ac7af47f9c9514773f393e7fb589

---
+++ src/scripts/Components/Popup.js
@@ -0,0 +1,40 @@
+import { config } from "../data.js";
+
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+    this._popupCloseButton = this._popup.querySelector(config.popupCloseButtonSelector);
+  }
+
+  open() {
+    this._popup.classList.remove(config.popupIsHiddenClass);
+    this._popup.classList.add(config.popupIsVisibleClass);
+    document.addEventListener("keydown", this._handleEscClose);
+    this._setEventListeners();
+  }
+
+  close() {
+    this._popup.classList.remove(config.popupIsVisibleClass);
+    this._popup.classList.add(config.popupIsHiddenClass);
+    document.removeEventListener("keydown", this._handleEscClose);
+  }
+
+  _handleEscClose = (e) => {
+    const isEscapeKey = e.key === "Escape";
+    if (isEscapeKey) {
+      this.close();
+    }
+  };
+
+  _setEventListeners() {
+    this._popupCloseButton.addEventListener("click", (e) => {
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+      this.close();
+    });
+    this._popup.addEventListener("click", (e) => {
+      const isModal = this._popup === e.target;
+      if (isModal) this.close();
+    });
+  }
+}
---
+++ src/scripts/utils.js
@@ -0,0 +1,33 @@
+import api from "./Components/API.js";
+
+const changeLikeIconState = (pointClicked, isLiked, cardId) => {
+  if (isLiked) {
+    api.removeLike(cardId).then((card) => {
+      pointClicked.src = "./src/images/heart.svg";
+      pointClicked.setAttribute("data-isLiked", "false");
+    });
+  } else {
+    api.addLike(cardId).then((card) => {
+      pointClicked.src = "./src/images/heart-liked.svg";
+      pointClicked.setAttribute("data-isLiked", "true");
+    });
+  }
+};
+
+export const manageCardController = (pointClicked, PopupDeleteConfirmation, cardId) => {
+  const isLikeIconClicked = pointClicked.classList[0].includes("like");
+  const isLiked = "true" === pointClicked.getAttribute("data-isLiked");
+  const isDeleteIconClicked = pointClicked.classList[0].includes("delete");
+  const isImageClicked = pointClicked.classList[0].includes("image");
+
+  if (isLikeIconClicked) {
+    changeLikeIconState(pointClicked, isLiked, cardId);
+  }
+  if (isDeleteIconClicked) {
+    PopupDeleteConfirmation.open(cardId, pointClicked);
+  }
+  if (isImageClicked) {
+    return true;
+  }
+  return false;
+};
